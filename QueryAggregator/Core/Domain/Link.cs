12a9f95b40283da8ce46ceb9501ade9bc9b751ac
oybekabdullaev <o.abdullaev2@student.inha.uz>
2020-12-10 16:49:36 +0500

    added models

---
+++ QueryAggregator/Core/Domain/Link.cs
@@ -0,0 +1,10 @@
+﻿namespace QueryAggregator.Core.Domain
+{
+    public class Link
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ QueryAggregator/Core/Domain/Query.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QueryAggregator.Core.Domain
+{
+    public class Query
+    {
+        public int Id { get; set; }
+        public string QueryString { get; set; }
+        public List<Link> Links { get; set; }
+    }
+}
